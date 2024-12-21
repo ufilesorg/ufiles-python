9c08b53d6427578f09ca37dc2b9bbe50c77250f3
Mahdi Kiani <mahdikiany@gmail.com>
2024-12-21 07:24:46 +0000

    permissionenum

--- src/ufiles/__init__.py
+++ src/ufiles/__init__.py
@@ -1,5 +1,5 @@
 from .async_ufiles import AsyncUFiles
-from .schemas import UFileItem
+from .schemas import UFileItem, PermissionEnum, PermissionSchema
 from .ufiles import UFiles
 
-__all__ = ["UFiles", "AsyncUssoSession", "UFileItem"]
+__all__ = ["UFiles", "AsyncUFiles", "UFileItem", "PermissionEnum", "PermissionSchema"]
